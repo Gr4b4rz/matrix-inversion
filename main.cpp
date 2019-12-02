24a200419be90b0e1105d5f9e4477f396d55a8cc
Paweł Zych <pawel1.zych@gmail.com>
2019-12-02 23:37:24 +0100

    changed to release

--- main.cpp
+++ main.cpp
@@ -4,10 +4,6 @@
 #include <thread>
 #include "Matrix.h"
 
-void multiply_matrices1(const matrix &a_matrix, const matrix &b_matrix,
-                        matrix &m_matrix, int start, int end);
-matrix multiply_parallel(const matrix &a_matrix, const matrix &b_matrix);
-
 int main(int argc, char **argv) {
     int dim;
     if (argc != 2 || atoi(argv[1]) < 1)
@@ -15,8 +11,6 @@ int main(int argc, char **argv) {
     else
         dim = atoi(argv[1]);
 
-    std::cout << "Suggested number of threads = " <<
-    std::thread::hardware_concurrency() << std::endl;
     matrix random_matrix = create_random_matrix(dim);
     matrix m = inverse_matrix_iterative(random_matrix);
     std::cout << "Matrix has been successfully inversed" << std::endl;
